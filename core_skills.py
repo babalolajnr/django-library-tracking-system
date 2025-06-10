b8a6105f4cbde9cbbc90c31934085c9dab1c77c9
Abdulqudduus Babalola <babalolajnr@gmail.com>
2025-06-10 10:55:04 +0100

    chore: prep

--- core_skills.py
+++ core_skills.py
@@ -1,9 +1,5 @@
-import random
-rand_list = [random.randint(1, 100) for _ in range(10)]
-print(rand_list)
+# rand_list =
 
-list_comprehension_below_10 = [num for num in rand_list if num < 10]
-print(list_comprehension_below_10)
+# list_comprehension_below_10 =
 
-list_comprehension_above_10 = [num for num in rand_list if num > 10]
-print(list_comprehension_above_10)
+# list_comprehension_above_10 =
